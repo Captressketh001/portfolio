fe37161ca3d23de350ddb1f65f41a39c8fedf910
captressketh001 <shininglite2012@gmail.com>
2021-05-07 12:15:06 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+!(function($) {
+    "use strict";
+  
+    // Hero typed
+    if ($('.typed').length) {
+      var typed_strings = $(".typed").data('typed-items');
+      typed_strings = typed_strings.split(',')
+      new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+      });
+    $(".toggle").on("click", function(){
+        if($(".item").hasClass("active")){
+            $(".item").removeClass("active");
+        }
+        else{
+            $(".item").addClass("active");
+        }
+    })
+  }
+})
+
+(jQuery);
